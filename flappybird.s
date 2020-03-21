f7f7ffdf495af77cba87748bfe1dbb56e269dcfb
Zain <s.basit@mail.utoronto.ca>
2020-03-20 20:58:30 -0400

    Pushing first file

---
+++ flappybird.s
@@ -0,0 +1,32 @@
+#####################################################################
+#
+# CSC258H5S Winter 2020 Assembly Programming Project
+# University of Toronto Mississauga
+#
+# Group members:
+# - Student 1: Syed Zain Basit, 1004900752
+# - Student 2: Alex Ng, Student Number
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 8					     
+# - Unit height in pixels: 8
+# - Display width in pixels: 256
+# - Display height in pixels: 256
+# - Base Address for Display: 0x10008000 ($gp)
+#
+# Which milestone is reached in this submission?
+# (See the assignment handout for descriptions of the milestones)
+# - Milestone 1/2/3/4/5 (choose the one the applies)
+#
+# Which approved additional features have been implemented?
+# (See the assignment handout for the list of additional features)
+# 1. (fill in the feature, if any)
+# 2. (fill in the feature, if any)
+# 3. (fill in the feature, if any)
+# ... (add more if necessary)
+#
+# Any additional information that the TA needs to know:
+# - (write here, if any)
+#
+#####################################################################
+                    
